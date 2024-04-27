594cecdb6264f7f0f7a14ed3fbc4ee299b7618d8
deoya <144986399+deoya@users.noreply.github.com>
2024-04-27 17:13:32 +0900

    섬네일 저장, 다수의 파일 저장 기능 추가

--- admin page/공지사항/NoticeMapper.java
+++ admin page/공지사항/NoticeMapper.java
@@ -3,6 +3,7 @@ package folio.port.mapper;
 import java.util.List;
 import java.util.Map;
 
+import folio.port.domain.AttachDAO;
 import folio.port.domain.infoDAO;
 import folio.port.domain.noticeDAO;
 
@@ -20,4 +21,11 @@ public interface NoticeMapper {
 	public List<noticeDAO> select_search(String search);
 	
 	public List<noticeDAO> select_search_pg(Map<String, Object> sh);
+	
+	public int filedata(AttachDAO dto);
+	
+	public int delete_file(int nidx);
+	
+	public List<AttachDAO> filesget(int nidx);
+	
 }
