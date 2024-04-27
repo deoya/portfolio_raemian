c0b04c713d41dc075534bc322c983b9fb9e36de5
deoya <144986399+deoya@users.noreply.github.com>
2024-04-27 17:00:29 +0900

    섬네일 저장 기능 추가
    - AJAX를 통한 섬네일 저장 기능 추가

--- admin page/공지사항/NoticeController.java
+++ admin page/공지사항/NoticeController.java
@@ -1,8 +1,14 @@
 package folio.port.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.print.attribute.standard.Media;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import folio.port.domain.AttachDAO;
 import folio.port.domain.noticeDAO;
 import folio.port.module.module2;
 import folio.port.service.NoticeService;
@@ -44,18 +51,28 @@ public class NoticeController {
 			m.addAttribute("All_list",service.select_search(search)); 
 		}
 		m.addAttribute("search", search);
+		
+		
+		
+		
+		
 	}
 	//--[단일 데이터 출력]------------------
 	@GetMapping("/notice_view/{nidx}")
 	public String notice_view(@PathVariable int nidx, Model m) {
 		m.addAttribute("data", service.get(nidx));
+		m.addAttribute("files",service.filesget(nidx));
+		
 		return "/raemian_admin/notice/notice_view";
 	}
 	
 	//--[글 등록]------------------
 	@PostMapping("/insert")
-	public String insert(@ModelAttribute noticeDAO dao, MultipartFile file, Model m) {
-		m.addAttribute("result",service.insert(dao, file));
+	public String insert(@ModelAttribute noticeDAO dao,
+			@RequestParam(required = false) String[] files, @RequestParam(required = false) String[] uploadPaths,
+			@RequestParam(required = false) String[] uuids, @RequestParam(required = false) String[] types,
+			Model m) {
+		m.addAttribute("result",service.insert(dao, files, uploadPaths, uuids, types));
 		return "resultpage";
 	}
 	
@@ -64,7 +81,24 @@ public class NoticeController {
 	@ResponseBody
 	public String delete(@RequestParam("nidx") int nidx,
 			@RequestParam(value="path", required = false, defaultValue = "") String path,
-			@RequestParam(value="nfile", required = false, defaultValue = "") String nfile) {
-		return service.delete(nidx,nfile, path);
+			@RequestParam(value="nfile", required = false, defaultValue = "") String nfile,
+			@RequestParam(value="filetype", required = false, defaultValue = "") String filetype
+			) {
+		return service.delete(nidx,nfile,filetype, path);
 	}
+	
+	//--[섬네일 저장용 ]------------------
+
+	@PostMapping(value="/fileajax", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public  ResponseEntity<List<AttachDAO>> fileajax(MultipartFile[] ajaxfile, Model m) {
+		
+		log.info(ajaxfile);
+		
+		return new ResponseEntity<>(service.fileajax(ajaxfile),  HttpStatus.OK);
+	}
+	
+	
+	
+	
 }
