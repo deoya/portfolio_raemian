cfd7b68788ea4eeb3a588d6c04de19c377a64cee
deoya <144986399+deoya@users.noreply.github.com>
2024-04-27 17:05:45 +0900

    섬네일 저장 기능,  다수의 파일 저장 기능 추가
    - 섬네일 AJAX로 저장 및 출력
    - 3개까지 파일 멀티 저장 가능

--- admin page/공지사항/notice_write.js
+++ admin page/공지사항/notice_write.js
@@ -1,3 +1,5 @@
+
+
 window.onload = function(){ 
 	var ck = CKEDITOR.replace("incontent",{
 		height:395
@@ -12,20 +14,58 @@ var html = new Vue({
 		filecked : "yes",
 	},
 	methods:{
-		fileck : function(data){
-			// 파일 확장자 제한
-			var regex = new RegExp("(.*?)\.(exe|sh|zip|alz|pdf)$");
-			if(data.target.files[0] != null) {
-				if(data.target.files[0].size > this.maxSize){
-					this.msg = "파일은 2MB 이하만 등록이 가능합니다"
-					this.filecked = "no"
+		fileajax : function(data){
+			if(data.target.files.length > 3){
+				alert("파일은 총 3개까지 업로드가 가능합니다")
+				f.ajaxfile.value = "";
+			}else {
+				if(data.target.files[0] == undefined){ // 파일 없앴을 시 리셋
+					document.getElementById("fileul").innerHTML = "";
 				}
-				else if(regex.test(data.target.files[0].name)){
-					this.msg = "해당 파일은 등록이 불가능 합니다"
-					this.filecked = "no"
-				}else {
-					this.filecked = "yes"
-				}	
+				// 파일 확장자 제한
+				var regex = new RegExp("(.*?)\.(exe|sh|zip|alz|pdf)$");
+				if(data.target.files[0] != null) {
+					if(data.target.files[0].size > this.maxSize){
+						this.msg = "파일은 2MB 이하만 등록이 가능합니다"
+						this.filecked = "no"
+					}
+					else if(regex.test(data.target.files[0].name)){
+						this.msg = "해당 파일은 등록이 불가능 합니다"
+						this.filecked = "no"
+					}else {
+						this.filecked = "yes"
+						
+						fetch(path+"/raemian_admin/notice/fileajax",{
+							method:'post',
+							body: new FormData(document.getElementById('f')) 
+						}).then(function(file){
+							return file.json()
+						}).then(function(file){
+							//jsp에 추가
+							var i = 0;
+							var s = "";
+							while(i < file.length){
+								if(file[i]["filetype"] ==  "I"){
+									s += "<li>"
+									  + "<img src='http://deoya.cdn1.cafe24.com/"+file[i]["uploadPath"]+"s_"+file[i]["uuid"]+file[i]["nfile"]+"'>"
+									  + "<input type='hidden' name='uploadPaths' value='"+file[i]["uploadPath"]+"'>"
+									  + "<input type='hidden' name='uuids' value='"+file[i]["uuid"]+"'>"
+									  + "<input type='hidden' name='files' value='"+file[i]["nfile"]+"'>"
+									  + "<input type='hidden' name='types' value='"+file[i]["filetype"]+"'>"
+									  + "</li>"
+								}else {
+									s += "<li><img src='/r/resources/img/attach.png'></li>"
+								}
+								i++;
+							}
+							
+							document.getElementById("fileul").innerHTML = s;
+							
+						}).catch(function(e){
+							alert("오류가 발생했습니다. 다음에 다시 시도하여주세요 [번호 : 044]")
+						})
+					}	
+				}
 			}
 		},
 		insert:function(){
